f0408a4e700f6f0d34dccc0f089c94e38e2d016e
Mike <mikewrubel51@gmail.com>
2021-02-25 19:52:20 -0500

    commit tree d3dfa97189ab99583c82b77aa0d31b978362df1f

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(speech = "HI GRANDMA")
+    if speech == speech.upcase
+        puts "NO, NOT SINCE 1938!"
+    elseif speech == "I LOVE YOU GRANDMA!"
+        puts "I LOVE YOU TOO PUMPKIN!"
+    else
+        puts "HUH?! SPEAK UP, SONNY!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
